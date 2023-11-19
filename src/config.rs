54f37bb07fdc65fab679b664590e5ef9cb8f4166
mochenya <2827075398@qq.com>
2023-11-19 12:11:59 +0800

    clean code

--- src/config.rs
+++ src/config.rs
@@ -27,12 +27,12 @@ impl Config {
         let exe_path = env::current_exe().unwrap();
         let mut config_file_path = exe_path.parent().unwrap().to_path_buf();
         config_file_path.push("luster_config.json");
-        // 读取json配置文件
+        // read config
         let config_json = JSON::read_json(
             config_file_path.to_str().unwrap(),
             &json!({"version": "0.1.0"}).to_string(),
         );
-        // 反序列化配置
+        // parse config
         let config: Config = serde_json::from_str(&config_json).unwrap();
         config
     }
@@ -41,7 +41,7 @@ impl Config {
         let exe_path = env::current_exe().unwrap();
         let mut config_file_path = exe_path.parent().unwrap().to_path_buf();
         config_file_path.push("luster_config.json");
-        // 写入配置
+        // save config
         JSON::save_json(
             config_file_path.to_str().unwrap(),
             &serde_json::to_string(self).unwrap(),
